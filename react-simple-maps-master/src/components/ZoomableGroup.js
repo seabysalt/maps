fdf84a292800bb534d8a57f638fb0e94e1fa98d2
seabysalt <seabysalt@gmail.com>
2020-04-28 19:13:04 +0200

    changed

---
+++ react-simple-maps-master/src/components/ZoomableGroup.js
@@ -0,0 +1,55 @@
+
+import React, { useContext } from "react"
+import PropTypes from "prop-types"
+
+import { MapContext } from "./MapProvider"
+import useZoomPan from "./useZoomPan"
+
+const ZoomableGroup = ({
+  center = [0, 0],
+  zoom = 1,
+  minZoom = 1,
+  maxZoom = 8,
+  translateExtent,
+  onMoveStart,
+  onMove,
+  onMoveEnd,
+  className,
+  ...restProps
+}) => {
+  const { width, height } = useContext(MapContext)
+
+  const {
+    mapRef,
+    transformString,
+  } = useZoomPan({
+    center,
+    onMoveStart,
+    onMove,
+    onMoveEnd,
+    scaleExtent: [minZoom, maxZoom],
+    translateExtent,
+    zoom,
+  })
+
+  return (
+    <g ref={mapRef}>
+      <rect width={width} height={height} fill="transparent" />
+      <g transform={transformString} className={`rsm-zoomable-group ${className}`} {...restProps} />
+    </g>
+  )
+}
+
+ZoomableGroup.propTypes = {
+  center: PropTypes.array,
+  zoom: PropTypes.number,
+  minZoom: PropTypes.number,
+  maxZoom: PropTypes.number,
+  translateExtent: PropTypes.arrayOf(PropTypes.array),
+  onMoveStart: PropTypes.func,
+  onMove: PropTypes.func,
+  onMoveEnd: PropTypes.func,
+  className: PropTypes.string,
+}
+
+export default ZoomableGroup
